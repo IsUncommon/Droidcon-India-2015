abb67d0c94f6cc76b4ad2c8b7eea6202b06a80ca
Pratul Kalia <pratul@pratul.in>
2015-11-26 19:45:27 +0530

    Added a CollapsingToolbarLayout to the SectionDetailActivity.

--- Workshop/src/main/java/is/uncommon/droidcon2015/SectionDetailActivity.java
+++ Workshop/src/main/java/is/uncommon/droidcon2015/SectionDetailActivity.java
@@ -2,11 +2,36 @@ package is.uncommon.droidcon2015;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class SectionDetailActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_detail);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
