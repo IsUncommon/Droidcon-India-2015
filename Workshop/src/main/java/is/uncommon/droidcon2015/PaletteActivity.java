782a676ac52957f61180c1d499826a485d14b875
Pratul Kalia <pratul@pratul.in>
2015-12-02 20:29:19 +0530

    Added Palette functioning.

--- Workshop/src/main/java/is/uncommon/droidcon2015/PaletteActivity.java
+++ Workshop/src/main/java/is/uncommon/droidcon2015/PaletteActivity.java
@@ -1,24 +1,40 @@
 package is.uncommon.droidcon2015;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.WeakHashMap;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class PaletteActivity extends AppCompatActivity {
+public class PaletteActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
 
     @Bind(R.id.toolbar) Toolbar mToolbar;
     @Bind(R.id.vp_palette) ViewPager mViewPager;
+    @Bind(R.id.tv_palette_head) TextView mHeadingTextView;
+    @Bind(R.id.tv_palette_subhead) TextView mSubheadingTextView;
+    @Bind(R.id.tv_palette_body) TextView mBodyTextView;
+    @Bind(R.id.btn_vibrant) Button mVibrantButton;
+    @Bind(R.id.btn_muted) Button mMutedButton;
 
-    private static final int[] IMAGES = { R.drawable.palette1, R.drawable.palette2, R.drawable.palette3, R.drawable.palette4 };
+    private static final int[] IMAGES = { R.drawable.palette5, R.drawable.palette6, R.drawable.palette3, R.drawable.palette4 };
+    private WeakHashMap<Bitmap, Palette> mBitmapPaletteMap;
+    private boolean isVibrantSelected = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +47,84 @@ public class PaletteActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Palette.");
 
+        mBitmapPaletteMap = new WeakHashMap<>();
+
+        mViewPager.addOnPageChangeListener(this);
         mViewPager.setAdapter(new PalettePagerAdapter());
+        onPageSelected(mViewPager.getCurrentItem());
+
+        if (isVibrantSelected) {
+            mVibrantButton.setSelected(true);
+            mMutedButton.setSelected(false);
+        } else {
+            mVibrantButton.setSelected(false);
+            mMutedButton.setSelected(true);
+        }
+    }
+
+    @OnClick(R.id.btn_vibrant)
+    public void onClickVibrantButton() {
+        isVibrantSelected = true;
+        mVibrantButton.setSelected(true);
+        mMutedButton.setSelected(false);
+        onPageSelected(mViewPager.getCurrentItem());
+    }
+
+    @OnClick(R.id.btn_muted)
+    public void onClickMutedButton() {
+        isVibrantSelected = false;
+        mVibrantButton.setSelected(false);
+        mMutedButton.setSelected(true);
+        onPageSelected(mViewPager.getCurrentItem());
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        Bitmap bitmap = ((BitmapDrawable) ContextCompat.getDrawable(this, IMAGES[position])).getBitmap();
+        if (mBitmapPaletteMap.containsKey(bitmap)) {
+            colorTextUsingPalette(mBitmapPaletteMap.get(bitmap));
+        } else {
+            generatePalette(bitmap);
+        }
+    }
+
+    private void colorTextUsingPalette(Palette palette) {
+        int vibrantHeadColor = palette.getVibrantColor(0xffbbbbbb);
+        int vibrantSubheadColor = palette.getDarkVibrantColor(0xffbbbbbb);
+        int vibrantBodyColor = palette.getLightVibrantColor(0xffbbbbbb);
+        int mutedHeadColor = palette.getMutedColor(0xffbbbbbb);
+        int mutedSubheadColor = palette.getDarkMutedColor(0xffbbbbbb);
+        int mutedBodyColor = palette.getLightMutedColor(0xffbbbbbb);
+
+        if (isVibrantSelected) {
+            mHeadingTextView.setTextColor(vibrantHeadColor);
+            mSubheadingTextView.setTextColor(vibrantSubheadColor);
+            mBodyTextView.setTextColor(vibrantBodyColor);
+        } else {
+            mHeadingTextView.setTextColor(mutedHeadColor);
+            mSubheadingTextView.setTextColor(mutedSubheadColor);
+            mBodyTextView.setTextColor(mutedBodyColor);
+        }
+    }
+
+    private void generatePalette(final Bitmap bitmap) {
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                mBitmapPaletteMap.put(bitmap, palette);
+                colorTextUsingPalette(palette);
+            }
+        });
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
     }
 
     protected class PalettePagerAdapter extends PagerAdapter {
