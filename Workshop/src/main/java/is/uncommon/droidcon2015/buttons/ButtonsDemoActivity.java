827bddbc291c8b537334873f80a31919b66e1446
Madhu <madhu@uncommon.is>
2015-12-03 22:58:23 +0530

    Add button demos for flat, floating and raised

---
+++ Workshop/src/main/java/is/uncommon/droidcon2015/buttons/ButtonsDemoActivity.java
@@ -0,0 +1,118 @@
+package is.uncommon.droidcon2015.buttons;
+
+import android.animation.Animator;
+import android.animation.ValueAnimator;
+import android.content.res.ColorStateList;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import is.uncommon.droidcon2015.R;
+import is.uncommon.droidcon2015.utils.HtmlUtils;
+
+/**
+ * Created by Madhu on 02/12/15.
+ */
+public class ButtonsDemoActivity extends AppCompatActivity implements TintRowViewHolder.TintRowSelectedListener {
+
+    @Bind(R.id.raised_button_desc) TextView mRaisedButtonDesc;
+    @Bind(R.id.fab_button_desc) TextView mFabButtonDesc;
+    @Bind(R.id.demo_button) AppCompatButton mDemoButton;
+    @Bind(R.id.tint_row) LinearLayout mTintLayout;
+    @Bind(R.id.fab) FloatingActionButton mFab;
+    @Bind(R.id.appbar) AppBarLayout mAppbar;
+    @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.demo_button_flat) AppCompatButton mDemoFlatButton;
+    @Bind(R.id.flat_button_desc) TextView mFlatButtonDescription;
+    private int mCurrentTintColor = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons_demo);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(R.string.demo_buttons);
+        HtmlUtils.setHtmlText(mRaisedButtonDesc, getString(R.string.raised_button_description));
+        HtmlUtils.setHtmlText(mFabButtonDesc, getString(R.string.fab_description));
+        HtmlUtils.setHtmlText(mFlatButtonDescription, getString(R.string.borderless_button_description));
+        TintRowViewHolder holder = TintRowViewHolder.getInstance(mTintLayout);
+        holder.setOnTintRowSelectedListener(this);
+        onTintSelected(holder.getSelectedColor());
+    }
+
+    @Override
+    public void onTintSelected(final int color) {
+        if (mCurrentTintColor == -1) {
+            changeTintColor(color);
+            mCurrentTintColor = color;
+        } else {
+            //animate tint color
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                ValueAnimator animator = ValueAnimator.ofArgb(mCurrentTintColor, color);
+                animator.setInterpolator(new AccelerateInterpolator());
+                animator.setDuration(300);
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                        changeTintColor((Integer) valueAnimator.getAnimatedValue());
+                    }
+                });
+                animator.start();
+                animator.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        changeTintColor(color);
+                        mCurrentTintColor = color;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+                        changeTintColor(color);
+                        mCurrentTintColor = color;
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+
+                    }
+                });
+            } else {
+                changeTintColor(color);
+                mCurrentTintColor = color;
+            }
+        }
+    }
+
+    private void changeTintColor(int color) {
+        ColorStateList colorState = ColorStateList.valueOf(color);
+        mDemoButton.setSupportBackgroundTintList(colorState);
+        ViewCompat.setBackgroundTintList(mFab, colorState);
+        ViewCompat.setBackgroundTintList(mAppbar, colorState);
+        ViewCompat.setBackgroundTintList(mToolbar, colorState);
+        mDemoFlatButton.setTextColor(colorState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(color);
+        }
+    }
+}
---
+++ Workshop/src/main/java/is/uncommon/droidcon2015/buttons/TintRowViewHolder.java
@@ -0,0 +1,87 @@
+package is.uncommon.droidcon2015.buttons;
+
+import android.graphics.Color;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import is.uncommon.droidcon2015.R;
+
+/**
+ * Created by Madhu on 03/12/15.
+ */
+public class TintRowViewHolder implements View.OnClickListener {
+    private static final int[] COLORS = new int[] {
+            Color.parseColor("#9C27B0"),
+            Color.parseColor("#2196F3"),
+            Color.parseColor("#009688"),
+            Color.parseColor("#E91E63"),
+            Color.parseColor("#FF9800")
+    };
+
+    private List<CardView> mCards = new ArrayList<>();
+    private List<ImageView> mCheckmarks = new ArrayList<>();
+    private TintRowSelectedListener onTintRowSelectedListener = null;
+
+    public static TintRowViewHolder getInstance(LinearLayout layoutView) {
+        TintRowViewHolder viewHolder = new TintRowViewHolder();
+
+        for (int i = 0; i < layoutView.getChildCount(); i++) {
+            View v = layoutView.getChildAt(i);
+            CardView iv = (CardView) v;
+            ImageView checkMark = (ImageView) v.findViewById(R.id.tint_row_check);
+            viewHolder.mCards.add(iv);
+            viewHolder.mCheckmarks.add(checkMark);
+            iv.setCardBackgroundColor(COLORS[i]);
+            if (i != 0) {
+                checkMark.setVisibility(View.GONE);
+            }
+            v.setOnClickListener(viewHolder);
+        }
+        return viewHolder;
+    }
+
+    public void setOnTintRowSelectedListener(TintRowSelectedListener listener) {
+        this.onTintRowSelectedListener = listener;
+    }
+
+    public int getSelectedColor() {
+        for (int i = 0; i < mCheckmarks.size(); i++) {
+            if (mCheckmarks.get(i).getVisibility() == View.VISIBLE) {
+                return COLORS[i];
+            }
+        }
+        return COLORS[0];
+    }
+
+    @Override
+    public void onClick(View view) {
+        int selected = 0;
+        for (int i = 0; i < mCards.size(); i++) {
+            if (mCards.get(i) == view) {
+                setItemSelected(i);
+                selected = i;
+                break;
+            }
+        }
+        onTintRowSelectedListener.onTintSelected(COLORS[selected]);
+    }
+
+    private void setItemSelected(int itemSelected) {
+        for (int i = 0; i < mCheckmarks.size(); i++) {
+            if (i == itemSelected) {
+                mCheckmarks.get(i).setVisibility(View.VISIBLE);
+            } else {
+                mCheckmarks.get(i).setVisibility(View.GONE);
+            }
+        }
+    }
+
+    public static interface TintRowSelectedListener {
+        public void onTintSelected(int color);
+    }
+}
